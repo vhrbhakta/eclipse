2cf9fbbaff4fc9c65259a75ea8e84a72fdf07cc1
vhrbhakta <vhrbhakta@gmail.com>
2018-03-07 19:34:23 -0800

    First commit. Project started

---
+++ src/myAwesomeProject/helloGIT.java
@@ -0,0 +1,10 @@
+package myAwesomeProject;
+
+public class helloGIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
